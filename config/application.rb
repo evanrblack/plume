eea046b9c4b199ac978abec370494351419bf7c1
Evan Black <evan@erblack.com>
2016-11-05 09:37:09 -0500

    Set minitest spec style as default

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,10 @@ module Plume
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Set minitest spec style with no fixtures as default.
+    config.generators do |g|
+      g.test_framework :minitest, spec: true, fixture: false
+    end
   end
 end
