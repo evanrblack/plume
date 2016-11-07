eb292fa36275db0e284b4272f2abefd23d463c56
Evan Black <evan@erblack.com>
2016-11-07 15:03:45 -0600

    Add controller tests for logins

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,4 +5,18 @@ describe PagesController do
     get '/'
     assert_response :success
   end
+
+  it 'must be able to log in as a manager' do
+    sign_in logins(:manager_login)
+    get '/'
+    assert_response :success
+    assert_select 'h1', 'Dashboard'
+  end
+
+  it 'must be able to log in as a caregiver' do
+    sign_in logins(:caregiver_login)
+    get '/'
+    assert_response :success
+    assert_select 'h1', 'Dashboard'
+  end
 end
