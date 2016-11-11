e92fdbe74dbf50d1fb6ffac5ec907345a1b74748
Evan Black <evan@erblack.com>
2016-11-10 21:11:37 -0600

    Begin fleshing out shifts

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -11,8 +11,49 @@ class Shift < ApplicationRecord
   validates :start_time, inclusion: { in: 0..MINUTES_PER_DAY }
   validates :duration, inclusion: { in: 0..MINUTES_PER_WEEK }
 
+  before_validation :adjust_start_date
+  before_validation :adjust_end_date, if: :end_date
+  validate :end_date_greater_than_start_date
+
+  # Overlapping shifts may be a feature...?
+  # validate :wont_overlap
+
+  def split
+  end
+
   private
 
-  def no_overlap
+  # Move start_date up to first same day.
+  def adjust_start_date
+    prefDay = Shift.days[self.day]
+    curDay = (start_date.wday - 1) % 7
+    diff = (prefDay - curDay).abs
+    if prefDay < curDay
+      self.start_date += 7 - diff
+    else
+      self.start_date += diff
+    end
+  end
+
+  # Move end_date back to last same day.
+  def adjust_end_date
+    prefDay = Shift.days[self.day]
+    curDay = (end_date.wday - 1) % 7
+    diff = (prefDay - curDay).abs
+    if prefDay > curDay
+      self.end_date -= (7 - diff)
+    else
+      self.end_date -= diff
+    end
+  end
+
+  def end_date_greater_than_start_date
+    return if end_date.nil? || end_date >= start_date
+    errors.add(:end_date, 'must be greater than start date')
+  end
+
+  def wont_overlap
+    # 1D collision detection
+    # First check for collision by dates, then compare week.
   end
 end
--- test/models/shift_test.rb
+++ test/models/shift_test.rb
@@ -6,4 +6,25 @@ describe Shift do
   it 'must have a valid fixture' do
     shift.must_be :valid?
   end
+
+  it 'must prevent nonsensical dates' do
+    shift.start_date = Date.today
+    shift.end_date = Date.today - 1.day
+    shift.validate
+    shift.errors.keys.must_include :end_date
+  end
+
+  it 'must align start and end dates based on day' do
+    shift.day = 'tuesday'
+    shift.start_date = Date.new(2016, 11, 4)  # Friday
+    shift.end_date = Date.new(2016, 11, 9)  # Wednesday
+    shift.save
+    shift.start_date.must_equal Date.new(2016, 11, 8)
+    shift.start_date.must_equal Date.new(2016, 11, 8)
+    shift.start_date = Date.new(2016, 10, 27)  # Thursday
+    shift.end_date = Date.new(2016, 11, 15)  # Tuesday
+    shift.save
+    shift.start_date.must_equal Date.new(2016, 11, 1)
+    shift.end_date.must_equal Date.new(2016, 11, 15)
+  end
 end
