073420c051beb39b9b8b7df12dd4be86b16c9ed7
Evan Black <evan@erblack.com>
2016-12-20 14:56:57 -0600

    Fix auth redirect hell

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,15 @@ class ApplicationController < ActionController::Base
 
   # Handles redirection when not authorized
   rescue_from CanCan::AccessDenied do |exception|
-    #store_location_for(:login, request.path)
+    # store_location_for(:login, request.path)
     flash[:danger] = exception.message
     redirect_to new_login_session_path
   end
 
   # Redirect to attempted page or root path if there wasn't one
-  #def after_sign_in_path_for(_resource)
+  # def after_sign_in_path_for(_resource)
   #  session[:login_return_to] || root_path
-  #end
+  # end
 
   def current_user
     current_login.loginable if current_login
